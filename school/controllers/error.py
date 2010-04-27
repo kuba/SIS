53c61b50303eaf9e0ee09c16386a261d5370a0b1
Jakub Warmuz <jakub.warmuz@gmail.com>
2010-04-27 17:16:21 +0200

    Added custom error handling.
    Change default pylons error template to something more "generic".

--- school/controllers/error.py
+++ school/controllers/error.py
@@ -1,12 +1,12 @@
 import cgi
 
 from paste.urlparser import PkgResourcesParser
-from pylons import request
+from pylons import request, tmpl_context as c
 from pylons.controllers.util import forward
 from pylons.middleware import error_document_template
 from webhelpers.html.builder import literal
 
-from school.lib.base import BaseController
+from school.lib.base import BaseController, render
 
 class ErrorController(BaseController):
 
@@ -24,23 +24,6 @@ class ErrorController(BaseController):
         """Render the error document"""
         resp = request.environ.get('pylons.original_response')
         content = literal(resp.body) or cgi.escape(request.GET.get('message', ''))
-        page = error_document_template % \
-            dict(prefix=request.environ.get('SCRIPT_NAME', ''),
-                 code=cgi.escape(request.GET.get('code', str(resp.status_int))),
-                 message=content)
-        return page
-
-    def img(self, id):
-        """Serve Pylons' stock images"""
-        return self._serve_file('/'.join(['media/img', id]))
-
-    def style(self, id):
-        """Serve Pylons' stock stylesheets"""
-        return self._serve_file('/'.join(['media/style', id]))
-
-    def _serve_file(self, path):
-        """Call Paste's FileApp (a WSGI application) to serve the file
-        at the specified path
-        """
-        request.environ['PATH_INFO'] = '/%s' % path
-        return forward(PkgResourcesParser('pylons', 'pylons'))
+        c.code=cgi.escape(request.GET.get('code', str(resp.status_int)))
+        c.message=content
+        return render('error.xml')
