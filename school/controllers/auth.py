5f2d355108a46290622092a9e94c120fc456f6f6
Jakub Warmuz <jakub.warmuz@gmail.com>
2010-05-04 22:43:10 +0200

    Login form rendering.

--- school/controllers/auth.py
+++ school/controllers/auth.py
@@ -1,6 +1,6 @@
 import logging
 
-from pylons import request, response, session, tmpl_context as c
+from pylons import url, request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
 
 from school.lib.base import BaseController, render
@@ -9,12 +9,6 @@ log = logging.getLogger(__name__)
 
 class AuthController(BaseController):
 
-    def index(self):
-        # Return a rendered template
-        #return render('/auth.mako')
-        # or, return a response
-        return 'Hello World'
-
     def login(self):
-        came_from = str(request.params.get('came_from', '')) or '/'
-        return '<form method="post" action="/login_handler?came_from=%s"><input type="text" name="login" /><input type="pasword" name="password" /><input type="submit" /></form>' % came_from
+        c.came_from = str(request.params.get('came_from', '')) or url('/')
+        return render('auth/login_form.xml')
