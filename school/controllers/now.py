f484839ed320d0dbff8b1ae86abb28caa50bd404
Jakub Warmuz <jakub.warmuz@gmail.com>
2010-05-06 00:05:05 +0200

    Rewritten now module.

--- school/controllers/now.py
+++ school/controllers/now.py
@@ -1,54 +1,34 @@
-import re, types
 from datetime import time, datetime
 
 import logging
 log = logging.getLogger(__name__)
 
-from sqlalchemy import or_, and_
-from sqlalchemy.orm.exc import NoResultFound
-
 from pylons import request, response, session, tmpl_context as c
-from pylons import url
 from pylons.controllers.util import abort, redirect_to
 
 from school.lib.base import BaseController, render
-from school.model import Student, Lesson, Educator, Schedule
-from school.model import meta
-
-
-class NowError(Exception):
-    pass
-
-class MultiplePeopleFoundError(NowError):
-    def __init__(self, people):
-        super(Exception, self).__init__()
-        self.people = people
-
-class NoPersonFoundError(NowError):
-    pass
-
-class MultipleLessonsFound(NowError):
-    pass
-
-class NoLessonFound(NowError):
-    pass
-
-class TooEarlyError(NowError):
-    pass
-
-class TooLateError(NowError):
-    pass
+from school.model import Student, Educator, Schedule
+from school.model.meta import Session
 
 
 class NowController(BaseController):
 
     def index(self):
+        """
+        Render index page or redirect to now or now_id actions if surname is
+        available in the request's params.
+
+        """
         if 'surname' in request.params:
             redirect_to('now_name', surname=request.params['surname'])
         else:
             return render('now/index.xml')
 
     def current_order(self):
+        """
+        Return the current lesson order.
+
+        """
         schedule = [time(7,55),
                     time(8,55),
                     time(10,0),
@@ -58,71 +38,63 @@ class NowController(BaseController):
                     time(13,50),
                     time(14,45)]
 
-        today = datetime.today()
-        weekday = datetime.weekday(today)
-        now = datetime.time(today)
+        now = datetime.now().time()
 
-        if now < schedule[0]:
-            raise TooEarlyError("Before 8 o'clock, stupid!")
+        if now < schedule[0] or now > schedule[-1]:
+            return
 
         order = 0
-        for lorder, ltime in enumerate(schedule):
-            if ltime <= now and lorder + 1 < len(schedule) \
-                    and not schedule[lorder + 1] <= now:
-                order = lorder + 1
+        for t in schedule:
+            if now >= t:
+                order += 1
+            if now < t:
                 break
-
-        if order == 0:
-            raise TooLateError("Kidding me? After school, go and have fun!")
 
         return order
 
-    def _now_person(self, person, schedule):
-        order = self.current_order()
-        day = datetime.weekday(datetime.now())
-        return person.lesson(day, order, schedule.id)
-
     def now(self, surname):
+        """
+        Get the lesson for given person ``surname``.
+
+        """
+        current_order = self.current_order()
+        if current_order is None:
+            return render('now/now.xml')
+
+        today = datetime.weekday(datetime.today())
         schedule = Schedule.current()
+
         c.year = schedule.year
 
-        students = meta.Session.query(Student).\
+        students = Session.query(Student).\
                 filter(Student.last_name.like(surname)).all()
-        teachers = meta.Session.query(Educator).\
+        teachers = Session.query(Educator).\
                 filter(Educator.last_name.like(surname)).all()
         people = students + teachers
 
-        try:
-            if len(people) > 1:
-                raise MultiplePeopleFoundError(people)
-            elif len(people) == 0:
-                raise NoPersonFoundError
-        except MultiplePeopleFoundError as e:
-            c.people = e.people
+        if len(people) != 1:
+            c.people = people
             return render('now/list.xml')
-        except NoPersonFoundError:
-            return "No such person!"
 
-        try:
-            l = self._now_person(people[0], schedule)
-            if len(l) == 0:
-                return "No lesson"
-            c.lesson = l
-        except NowError as e:
-            return repr(e)
+        c.lesson = people[0].lesson(today, current_order, schedule.id)
         return render('now/now.xml')
 
     def now_id(self, id):
+        """
+        Get the lesson for given person ``id``.
+
+        """
+        current_order = self.current_order()
+        if current_order is None:
+            return render('now/now.xml')
+
+        today = datetime.weekday(datetime.today())
         schedule = Schedule.current()
         c.year = schedule.year
-        person = meta.Session.query(Student).get(id)
+
+        person = Session.query(Student).get(id)
         if not person:
-            person = meta.Session.query(Educator).get(id)
-        try:
-            l = self._now_person(person, schedule)
-            if len(l) == 0:
-                return "No lesson"
-            c.lesson = l
-        except NowError as e:
-            return repr(e)
+            person = Session.query(Educator).get(id)
+
+        c.lesson = person.lesson(today, current_order, schedule.id)
         return render('now/now.xml')
