1d885986c4b2645845e492d038ec1b17532638ec
Jakub Warmuz <jakub.warmuz@gmail.com>
2010-05-05 15:58:12 +0200

    Add current() and current_week() to LuckyNumber

--- school/model/lucky.py
+++ school/model/lucky.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 
 from sqlalchemy import Column, Integer, SmallInteger, Date
@@ -27,7 +28,103 @@ class LuckyNumber(Base):
         return "<%s(%s, %s)>" % (cls, self.date, self.number)
 
     @classmethod
+    def current(cls, change_hour, now=None):
+        """
+        Return current lucky number.
+
+        If the current hour is less than ``change_hour`` try to fetch lucky
+        number for the same date. Otherwise fetch the closest lucky number.
+
+        :param change_hour: The hour that defines the end of the day.
+        :type change_hour: :class:`int`
+
+        :param now: Fetch lucky number relatively to given datetime. If value
+                    is set to None it will use real current datetime.
+        :type now: :class:`datetime.datetime` or :class:`NoneType`
+
+        """
+        if now is None:
+            now = datetime.datetime.now()
+
+        if now.hour >= change_hour:
+            start_date = now + datetime.timedelta(1)
+        else:
+            start_date = now
+
+        lucky = Session.query(cls).\
+                    filter(cls.date >= start_date).\
+                    order_by(cls.date).first()
+        return lucky
+
+    @classmethod
+    def current_week(cls, change_hour, now=None):
+        """
+        Return ``current week``'s lucky numbers.
+
+        ``current week`` is defined based on the given ``now`` and
+        ``change_hour`` If there are no more lucky numbers in the
+        ``current week`` fetch lucky numbers from the next available week.
+
+        :param change_hour: The hour that defines the end of the day.
+        :type change_hour: :class:`int`
+
+        :param now: One of the ``current week``'s days. If value is set to
+                    None it will use real current datetime.
+        :type now: :class:`datetime.datetime` or :class:`NoneType`
+
+        """
+        if now is None:
+            now = datetime.datetime.now()
+        today = now.date()
+
+        if now.hour >= change_hour:
+            closest_day = today + datetime.timedelta(1)
+        else:
+            closest_day = now
+        closest_weekday = datetime.date.weekday(closest_day)
+
+        # Retrieve the date of the first day in the week
+        start_date = closest_day - datetime.timedelta(closest_weekday)
+        first_week_end = start_date + datetime.timedelta(7)
+        second_week_end = start_date + datetime.timedelta(14)
+
+        q = Session.query(cls).filter(cls.date >= start_date)
+
+        # Optmization. If the ``closest_day`` is 0 it is already new week,
+        # no need to query next two weeks, only one.
+        if closest_weekday == 0:
+            q = q.limit(7)
+        else:
+            q = q.limit(14)
+
+        # Fetch numbers from the database
+        numbers = q.all()
+
+        first_week = []
+        second_week = []
+
+        for number in numbers:
+            if number.date < first_week_end:
+                first_week.append(number)
+            else:
+                if len(second_week) == 0:
+                    second_week_end = number.date + datetime.timedelta(7)
+                if number.date < second_week_end:
+                    second_week.append(number)
+
+        # Check whether first week any more lucky numbers relatively to
+        # the current closest day.
+        if len(first_week) > 0 and first_week[-1].date >= closest_day:
+            return first_week
+        else:
+            return second_week
+
+    @classmethod
     def last(cls):
+        """
+        Return most recent lucky number.
+
+        """
         return Session.query(cls).order_by(desc(cls.date)).first()
 
     @classmethod
